ce518061e87bce7433b19d54113ea6393316207e
Ignacio Moscatelli <inm300300@gmail.com>
2025-04-27 22:14:33 -0300

    animacion bandera completa

---
+++ js/animacion-bandera.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', () => {
+    const gif = document.getElementById('bandera-san-luis');
+    const duracionGif = 850; // Duración en milisegundos (por ejemplo 3 segundos)
+
+    const srcActual = gif.src; // el src completo (con dominio y todo)
+  
+    // Cambiamos solo el nombre del archivo, mantenemos el path
+    const nuevoSrc = srcActual.replace('gif-bandera.gif', 'Bandera-pagina-web.png');
+  
+    setTimeout(() => {
+      gif.src = nuevoSrc;
+    }, duracionGif);
+});
